670e01073f8903c7071fb0e63474517fafe353f2
malfred <malenekf95@gmail.com>
2024-02-09 10:29:51 +0100

    creating repository for player

---
+++ src/EurovisionOnMars.Api/Repositories/IPlayerRespository.cs
@@ -0,0 +1,12 @@
+﻿using EurovisionOnMars.Entity;
+using System.Collections.Immutable;
+
+namespace EurovisionOnMars.Api.Repositories;
+
+public interface IPlayerRespository
+{
+    Task<ImmutableList<Player>> GetPlayers();
+    Task<Player?> GetPlayer(int id);
+    Task<Player?> GetPlayer(string username);
+    Task<Player> CreatePlayer(string username);
+}
---
+++ src/EurovisionOnMars.Api/Repositories/PlayerRepository.cs
@@ -0,0 +1,46 @@
+﻿using EurovisionOnMars.Entity;
+using EurovisionOnMars.Entity.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Immutable;
+
+namespace EurovisionOnMars.Api.Repositories;
+
+public class PlayerRepository : IPlayerRespository
+{
+    private readonly DataContext _context;
+    private readonly ILogger<PlayerRepository> _logger;
+
+    public PlayerRepository(DataContext context, ILogger<PlayerRepository> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    public async Task<ImmutableList<Player>> GetPlayers()
+    {
+        _logger.LogDebug("Getting all players");
+        var players = await _context.Players.ToListAsync();
+        return players.ToImmutableList();
+    }
+
+    public async Task<Player?> GetPlayer(int id)
+    {
+        _logger.LogDebug($"Getting player with id={id}");
+        return await _context.Players.FirstOrDefaultAsync(p => p.Id == id);
+    }
+
+    public async Task<Player?> GetPlayer(string username)
+    {
+        _logger.LogDebug($"Getting player with username={username}");
+        return await _context.Players.FirstOrDefaultAsync(p => p.Username == username);
+    }
+
+    public async Task<Player> CreatePlayer(string username)
+    {
+        _logger.LogDebug($"Creating player with {username}");
+        var newPlayer = new Player(username);
+        _context.Players.Add(newPlayer);
+        await _context.SaveChangesAsync();
+        return newPlayer;
+    }
+}
