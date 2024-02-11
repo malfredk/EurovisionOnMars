1fd150165aa818137166dee9592ecf81cbdb0b64
malfred <malenekf95@gmail.com>
2024-02-11 15:05:44 +0100

    moving interfaces to the same file as their implementation

--- src/EurovisionOnMars.Api/Repositories/PlayerRepository.cs
+++ src/EurovisionOnMars.Api/Repositories/PlayerRepository.cs
@@ -5,6 +5,14 @@ using System.Collections.Immutable;
 
 namespace EurovisionOnMars.Api.Repositories;
 
+public interface IPlayerRepository
+{
+    Task<ImmutableList<Player>> GetPlayers();
+    Task<Player?> GetPlayer(int id);
+    Task<Player?> GetPlayer(string username);
+    Task<Player> CreatePlayer(string username);
+}
+
 public class PlayerRepository : IPlayerRepository
 {
     private readonly DataContext _context;
--- src/EurovisionOnMars.Api/Services/PlayerService.cs
+++ src/EurovisionOnMars.Api/Services/PlayerService.cs
@@ -5,6 +5,14 @@ using System.Collections.Immutable;
 
 namespace EurovisionOnMars.Api.Services;
 
+public interface IPlayerService
+{
+    Task<ImmutableList<Player>> GetPlayers();
+    Task<Player> GetPlayer(int id);
+    Task<Player> GetPlayer(string username);
+    Task<Player> CreatePlayer(string username);
+}
+
 public class PlayerService : IPlayerService
 {
     private readonly IPlayerRepository _repository;
