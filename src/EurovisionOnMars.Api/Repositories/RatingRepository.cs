d4887a99c97b1e5f5b5f2af7910eecc779512689
malfred <malenekf95@gmail.com>
2024-02-11 15:42:18 +0100

    creating repository for rating

---
+++ src/EurovisionOnMars.Api/Repositories/RatingRepository.cs
@@ -0,0 +1,46 @@
+﻿using EurovisionOnMars.Entity;
+using EurovisionOnMars.Entity.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Immutable;
+
+namespace EurovisionOnMars.Api.Repositories;
+
+public interface IRatingRepository
+{
+    Task<ImmutableList<Rating>> GetRatingsByPlayer(int playerId);
+    Task<Rating?> GetRating(int id);
+    Task<Rating> UpdateRating(Rating rating);
+}
+
+public class RatingRepository : IRatingRepository
+{
+    private readonly DataContext _context;
+    private readonly ILogger<RatingRepository> _logger;
+
+    public RatingRepository(DataContext context, ILogger<RatingRepository> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    public async Task<ImmutableList<Rating>> GetRatingsByPlayer(int playerId)
+    {
+        _logger.LogDebug($"Getting all ratings for player with id={playerId}");
+        var ratings = await _context.Ratings.Where(r => r.PlayerId == playerId).ToListAsync();
+        return ratings.ToImmutableList();
+    }
+
+    public async Task<Rating?> GetRating(int id)
+    {
+        _logger.LogDebug($"Getting rating with id={id}");
+        return await _context.Ratings.FirstOrDefaultAsync(r => r.Id == id);
+    }
+
+    public async Task<Rating> UpdateRating(Rating rating)
+    {
+        _logger.LogDebug($"Updating rating with id={rating.Id}");
+        var updatedRating = _context.Update(rating);
+        await _context.SaveChangesAsync();
+        return updatedRating.Entity;
+    }
+}
