b9b8cb2d5856f88fc1d35e403d0bb50374124a39
malfred <malenekf95@gmail.com>
2024-02-11 12:22:10 +0100

    making list mapper more readable

--- src/EurovisionOnMars.Api/Mappers/Utils.cs
+++ src/EurovisionOnMars.Api/Mappers/Utils.cs
@@ -1,16 +1,13 @@
-﻿using EurovisionOnMars.Dto;
-using System.Collections.Generic;
-
-namespace EurovisionOnMars.Api.Mappers;
+﻿namespace EurovisionOnMars.Api.Mappers;
 
 public static class Utils
 {
-    public static List<B>? MapList<A, B>(List<A>? list, Func<A, B> mapper)
+    public static List<B>? MapList<A, B>(List<A>? entityList, Func<A, B> toDto)
     {
-        if (list == null)
+        if (entityList == null)
         {
             return null; 
         }
-        return list.Select(item =>  mapper(item)).ToList();
+        return entityList.Select(entity =>  toDto(entity)).ToList();
     }
 }
