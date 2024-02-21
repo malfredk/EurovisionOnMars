70d6531bb9314c2c359fd31e12a86ec4ebd75d6a
malfred <malenekf95@gmail.com>
2024-02-21 18:15:07 +0100

    creating country dto

---
+++ src/EurovisionOnMars.Dto/CountryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EurovisionOnMars.Dto;
+
+public record CountryDto : IdBaseDto
+{
+    public required int Number { get; set; }
+    public required string Name { get; set; }
+    public int? Ranking { get; set; }
+}
