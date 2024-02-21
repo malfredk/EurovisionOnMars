806971690fecc8ad10e8cf680d137723f22bdb67
malfred <malenekf95@gmail.com>
2024-02-21 18:20:45 +0100

    creating country mapper

---
+++ src/EurovisionOnMars.Api/Mappers/CountryMapper.cs
@@ -0,0 +1,23 @@
+﻿using EurovisionOnMars.Dto;
+using EurovisionOnMars.Entity;
+
+namespace EurovisionOnMars.Api.Mappers;
+
+public interface ICountryMapper
+{
+    public CountryDto ToDto(Country entity);
+}
+
+public class CountryMapper : ICountryMapper
+{
+    public CountryDto ToDto(Country entity)
+    {
+        return new CountryDto
+        {
+            Id = entity.Id,
+            Number = entity.Number,
+            Name = entity.Name,
+            Ranking = entity.Ranking
+        };
+    }
+}
