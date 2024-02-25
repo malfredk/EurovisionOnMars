f0e28b8dcd047ebad8a02cba5c2605bc0d34244c
malfred <malenekf95@gmail.com>
2024-02-25 16:12:48 +0100

    removing required modifier from foreign keys

--- src/EurovisionOnMars.Entity/Rating.cs
+++ src/EurovisionOnMars.Entity/Rating.cs
@@ -5,10 +5,10 @@ public record Rating : IdBase
     public int? Category1Points { get; set; }
     public int? Category2Points { get; set; }
     public int? Category3Points { get; set; }
-    public required int PlayerId { get; init; }
+    public int PlayerId { get; init; }
     public Player? Player { get; set; }
     public int? PointsSum { get; set; }
     public int? Ranking { get; set; }
-    public required int CountryId { get; init; }
+    public int CountryId { get; init; }
     public Country? Country { get; set; }
 }
