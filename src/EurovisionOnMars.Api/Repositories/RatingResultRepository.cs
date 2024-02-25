7ac8821a4bd9db0041378e745e3eb6762ddbf269
malfred <malenekf95@gmail.com>
2024-02-25 19:30:14 +0100

    creating repository for rating result

---
+++ src/EurovisionOnMars.Api/Repositories/RatingResultRepository.cs
@@ -0,0 +1,29 @@
+﻿using EurovisionOnMars.Entity;
+using EurovisionOnMars.Entity.DataAccess;
+
+namespace EurovisionOnMars.Api.Repositories;
+
+public interface IRatingResultRepository
+{
+    Task<RatingResult> UpdateRatingResult(RatingResult ratingResult);
+}
+
+public class RatingResultRepository : IRatingResultRepository
+{
+    private readonly DataContext _context;
+    private readonly ILogger<RatingResultRepository> _logger;
+
+    public RatingResultRepository(DataContext context, ILogger<RatingResultRepository> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    public async Task<RatingResult> UpdateRatingResult(RatingResult ratingResult)
+    {
+        _logger.LogDebug($"Updating ratingResult with id={ratingResult.Id}");
+        var updatedRatingResult = _context.Update(ratingResult);
+        await _context.SaveChangesAsync();
+        return updatedRatingResult.Entity;
+    }
+}
