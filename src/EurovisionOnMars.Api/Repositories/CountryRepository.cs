95f5172a86cdb8ec6cf91a2c0e50c1918f905ad9
malfred <malenekf95@gmail.com>
2024-03-05 14:02:09 +0100

    creating repository methods to create and update country

--- src/EurovisionOnMars.Api/Repositories/CountryRepository.cs
+++ src/EurovisionOnMars.Api/Repositories/CountryRepository.cs
@@ -8,6 +8,8 @@ namespace EurovisionOnMars.Api.Repositories;
 public interface ICountryRepository
 {
     Task<ImmutableList<Country>> GetCountries();
+    Task<Country> CreateCountry(Country country);
+    Task<Country> UpdateCountry(Country country);
 }
 
 public class CountryRepository : ICountryRepository
@@ -26,5 +28,21 @@ public class CountryRepository : ICountryRepository
         _logger.LogDebug("Getting all countries");
         var countries = await _context.Countries.ToListAsync();
         return countries.ToImmutableList();
+    }
+
+    public async Task<Country> CreateCountry(Country country)
+    {
+        _logger.LogDebug($"Creating country number {country.Number}");
+        _context.Countries.Add(country);
+        await _context.SaveChangesAsync();
+        return country;
+    }
+
+    public async Task<Country> UpdateCountry(Country country)
+    {
+        _logger.LogDebug($"Updating country with id={country.Id}");
+        var updatedCountry = _context.Update(country);
+        await _context.SaveChangesAsync();
+        return updatedCountry.Entity;
     }
 }
