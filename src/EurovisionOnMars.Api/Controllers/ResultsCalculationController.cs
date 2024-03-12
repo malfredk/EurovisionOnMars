3201fc1a2468da24d6d6c6ab5e75a427739f60c1
malfred <malenekf95@gmail.com>
2024-03-12 13:29:36 +0100

    creating result calculations controller

---
+++ src/EurovisionOnMars.Api/Controllers/ResultsCalculationController.cs
@@ -0,0 +1,25 @@
+﻿using EurovisionOnMars.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EurovisionOnMars.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ResultsCalculationController : ControllerBase
+{
+    private readonly IResultService _service;
+    private readonly ILogger<ResultsCalculationController> _logger;
+
+    public ResultsCalculationController(IResultService service, ILogger<ResultsCalculationController> logger)
+    {
+        _service = service;
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CalculateResults()
+    {
+        await _service.CalculateResults();
+        return Ok();
+    }
+}
