f9d8e4a9d22da2f4c266a855891f2a9f0c242a44
malfred <malenekf95@gmail.com>
2024-03-20 20:59:25 +0100

    setting base url in client for production

--- src/EurovisionOnMars.Wasm/Program.cs
+++ src/EurovisionOnMars.Wasm/Program.cs
@@ -8,7 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient {
-    BaseAddress = new Uri("https://localhost:7195") 
+    BaseAddress = new Uri(builder.HostEnvironment.IsDevelopment() ? 
+    "https://localhost:7195" : builder.HostEnvironment.BaseAddress) 
 });
 builder.Services.AddMudServices();
 
