b124ee8572ad1a6ff3092f1b0fcc96d129a92ba7
malfred <malenekf95@gmail.com>
2024-03-22 17:42:11 +0100

    removing hardcoded value for development base url

--- src/EurovisionOnMars.Wasm/Program.cs
+++ src/EurovisionOnMars.Wasm/Program.cs
@@ -8,8 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient {
-    BaseAddress = new Uri(builder.HostEnvironment.IsDevelopment() ? 
-    "https://localhost:7195" : builder.HostEnvironment.BaseAddress) 
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
 });
 builder.Services.AddMudServices();
 
