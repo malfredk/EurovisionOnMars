1a07f9e19f85c55b1cdc3cc3480657f0063b7a06
malfred <malenekf95@gmail.com>
2024-04-07 18:04:03 +0200

    changing config for snackbar alerts

--- src/EurovisionOnMars.Wasm/Program.cs
+++ src/EurovisionOnMars.Wasm/Program.cs
@@ -1,6 +1,7 @@
 using EurovisionOnMars.Wasm;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,6 +11,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
 });
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+    config.SnackbarConfiguration.ClearAfterNavigation = false;
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.MaxDisplayedSnackbars = 3;
+    config.SnackbarConfiguration.VisibleStateDuration = 2500;
+});
 
 await builder.Build().RunAsync();
