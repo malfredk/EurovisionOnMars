741e73260d59eb952c40a5506d9162c0d5b2a220
malfredk <malenekf95@gmail.com>
2025-12-22 17:35:18 +0100

    adding fields to prediction

--- src/EurovisionOnMars.Entity/Prediction.cs
+++ src/EurovisionOnMars.Entity/Prediction.cs
@@ -6,6 +6,8 @@ public record Prediction : IdBase
 {
     public int? TotalGivenPoints { get; private set; }
     public int? CalculatedRank { get; private set; }
+    public int? SameRankCount { get; private set; }
+    public int? TieBreakDemotion { get; private set; }
     public int PlayerRatingId { get; private set; }
     [JsonIgnore]
     public PlayerRating? PlayerRating { get; private set; }
@@ -33,8 +35,36 @@ public record Prediction : IdBase
     {
         if (rank < 1 || rank > 26)
         {
-            throw new ArgumentException("Rank must be between 1 and 26");
+            throw new ArgumentException("Rank must be at least 1 and no more than 26.");
         }
         CalculatedRank = rank;
+    }
+
+    public void SetSameRankCount(int sameRankCount)
+    {
+        if (sameRankCount < 0 || sameRankCount > 26)
+        {
+            throw new ArgumentException("SameRankCount should be positive and no more than 26.");
+        }
+        SameRankCount = sameRankCount;
+    }
+
+    public void SetTieBreakDemotion(int tieBreakDemotion)
+    {
+        if (!SameRankCount.HasValue || tieBreakDemotion < 0 || tieBreakDemotion >= SameRankCount)
+        {
+            throw new ArgumentException($"TieBreakDemotion must be positive and less than sameRankCount={SameRankCount}.");
+        }
+        TieBreakDemotion = tieBreakDemotion;
+    }
+
+    public int? GetFinalRank()
+    {
+        int? finalRank = null;
+        if (CalculatedRank.HasValue)
+        {
+            finalRank = CalculatedRank + (TieBreakDemotion ?? 0);
+        }
+        return finalRank;
     }
 }
