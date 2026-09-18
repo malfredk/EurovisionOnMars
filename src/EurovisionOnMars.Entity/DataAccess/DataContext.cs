f7bbfcdb010e5e6d51b505f3fe246379b03f000d
malfredk <malenekf95@gmail.com>
2026-09-18 20:02:16 +0200

    converting country position in data context

--- src/EurovisionOnMars.Entity/DataAccess/DataContext.cs
+++ src/EurovisionOnMars.Entity/DataAccess/DataContext.cs
@@ -25,9 +25,36 @@ public class DataContext : DbContext
 
     private static void ConfigureCountry(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Country>()
+        var country = modelBuilder.Entity<Country>();
+
+        country
             .HasIndex(country => country.Number)
             .IsUnique();
+
+
+        var countryNumberConverter = CreateCountryPositionConverter();
+        country
+            .Property(country => country.Number)
+            .HasConversion(countryNumberConverter);
+
+        var countryRankConverter = CreateNullableCountryPositionConverter();
+        country
+            .Property(country => country.ActualRank)
+            .HasConversion(countryRankConverter);
+    }
+
+    private static ValueConverter<CountryPosition, int> CreateCountryPositionConverter()
+    {
+        return new ValueConverter<CountryPosition, int>(
+            position => position.Value,
+            value => CountryPosition.Create(value));
+    }
+
+    private static ValueConverter<CountryPosition?, int?> CreateNullableCountryPositionConverter()
+    {
+        return new ValueConverter<CountryPosition?, int?>(
+            position => position == null ? null : position.Value,
+            value => value == null ? null : CountryPosition.Create(value.Value));
     }
 
     private static void ConfigurePlayer(ModelBuilder modelBuilder)
@@ -39,9 +66,9 @@ public class DataContext : DbContext
 
     private static void ConfigurePlayerRating(ModelBuilder modelBuilder)
     {
-        var pointsConverter = CreatePointsConverter();
-
         var playerRating = modelBuilder.Entity<PlayerRating>();
+
+        var pointsConverter = CreatePointsConverter();
 
         playerRating
             .Property(rating => rating.Category1Points)
